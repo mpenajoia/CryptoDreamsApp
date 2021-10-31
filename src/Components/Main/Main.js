1ccd782d61a648fa90b9ba34ecd22afee4a9b068
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2021-10-31 14:57:39 -0700

    Psuedocode written to attempt an added feature

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -6,32 +6,29 @@ import { useParams } from "react-router"
 
 const Main = (props) => {
     let { sym } = useParams();
-    // console.log('props on Main:', props.match.params.sym)
+
     const [coinList, setCoinList] = useState([])
     const makeApiCall = () => {
         fetch('https://api.coingecko.com/api/v3/coins/')
         .then(response => response.json())
         .then((data) => {
-            // console.log(data)
             setCoinList(data);
         })
     }
     useEffect(() => {
         makeApiCall()
     }, [])
-    // console.log(coinList[0])
+    const [addedCrypto, setAddedCrypto] = useState(sym)
 
     const defaultSideArray = coinList.filter((item) => {
         return item.symbol === 'btc' || item.symbol === 'eth' || item.symbol === 'sol' || item.symbol === 'ada' || item.symbol === 'dot'
     })
-    // console.log(defaultSideArray)
+    
 
     return (
         <div className="main">
-            <Sidebar defaultSideArray={defaultSideArray}/>
+            <Sidebar sym={sym} setAddedCrypto={setAddedCrypto} defaultSideArray={defaultSideArray}/>
             <Content sym={sym} defaultSideArray={defaultSideArray} handleMoney={props.handleMoney}/>
-
-            {/* <Route exact path="/:sym" render={(routerProps) => <Content {...routerProps} handleMoney={props.handleMoney}/>}/> */}
         </div>
     )
 }
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -3,20 +3,31 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const Sidebar = (props) => {
-    // console.log('sidebar props: ', props.defaultSideArray)
-
-    // make a useState that is an array of the current selection for the li's below similar to the example below
-    // const [defaultList, setDefaultList] = useState(['btc', 'eth', 'dot', 'sol', 'ada'])
-    // if I can pull off the add crypto feature, then i will need to use the spread operator on defaultList to make a copy of the default cryptos and with the addition of the user's additions save it to a new variable
-    // for example const newSideBarOptions = setDefaultList([...defaultList, added crypto])
-    // the li's below will be replaced with a function thats maps through the useState's array to list an li for each of the items in the array
-
-
     // fetch api call when user inputs from a search
+
+    // create an empty array (in a useState?) to store the incoming user searched cryptos
+    //this array will later be mapped through (after being validated) and will appear in the side bar
+
+    //validity of crypto will come from the api fetch's confirmation
 
     const sidebarMap = props.defaultSideArray.map(item => <li><Link to={"/" + item.id}><img src={item.image.small} alt={item.id}/><span className="sidebar-sym">{item.symbol.toUpperCase()}</span></Link></li>)
 
-    
+    const [inputCrypto, setInputCrypto] = useState();
+    const [searchCrypto, setSearchCrypto] = useState(false)
+    const handleAddCrypto = (event) => {
+        event.preventDefault();
+        setSearchCrypto(true)
+        
+    }
+    const handleCryptoChange = (event) => {
+        event.preventDefault();
+        setInputCrypto(event.target.value)
+    }
+    const handleSubmitCrypto = (event) => {
+        event.preventDefault();
+        setSearchCrypto(false)
+        console.log(inputCrypto)
+    }
 
     return (
         <div className="sidebar">
@@ -25,7 +36,14 @@ const Sidebar = (props) => {
                 {sidebarMap}
                 {/* render the second array thats mapped over of newly added cryto's */}
                 {/* hard code an add crypto button in an li */}
+                <li><button onClick={handleAddCrypto}>Add Me</button></li>
             </ul>
+            {searchCrypto ? 
+                <form onSubmit={handleSubmitCrypto}>
+                    <input onChange={handleCryptoChange} value={inputCrypto} type="text" placeholder="enter a cryptocurrency"/>
+                </form> 
+                : ''
+            }
         </div>
     )
 }
